96d76685c205614996371a45c8df69a15ef75ad7
Hannah Kates <hannah.b.kates@gmail.com>
2018-03-08 08:56:45 -0500

    added additional manual geocoding join

--- 08_geocode.sql
+++ 08_geocode.sql
@@ -107,6 +107,17 @@ WHERE
 	dobdev_jobs.the_geom IS NULL
 	AND dobdev_jobs.dob_job_number = b.dob_job_number::text;
 
+-- Final round of manual geocoding
+UPDATE dobdev_jobs
+SET
+	the_geom = ST_SetSRID(ST_MakePoint(b.long::numeric, b.lat::numeric),4326),
+	bin = b.bin,
+	bbl = b.bbl,
+	x_geomsource = (CASE WHEN b.label = 'Legacy' THEN 'Manual-BillLegacy' WHEN b.label = 'SamL-manual' THEN 'Manual-Sam' END)
+FROM dobdev_manualgeo_sl AS b
+WHERE
+	dobdev_jobs.the_geom IS NULL
+	AND dobdev_jobs.dob_job_number = b.dob_job_number::text;
 
 -- Reapply reapply previous geoms from points that were manually moved
 -- UPDATE dobdev_jobs

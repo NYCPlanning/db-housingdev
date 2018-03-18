6e2d76774232c98d64c80bd19c38ea3323c256ff
Hannah Kates <hannah.b.kates@gmail.com>
2018-03-18 10:54:39 -0400

    revised geom join sequencing

--- 08_geocode.sql
+++ 08_geocode.sql
@@ -1,114 +1,113 @@
 -- Clean up dummy BBLs and BINs
-UPDATE dobdev_jobs
+UPDATE dobdev_jobs_20180316
 SET bbl = NULL
 WHERE bbl = '0';
 
-UPDATE dobdev_jobs
+UPDATE dobdev_jobs_20180316
 SET bin = NULL
 WHERE bin = '0' OR RIGHT(bin, 6) = '000000';
 
--- Tags geoms that came from Jan 2017 data for tracing purposes
-UPDATE dobdev_jobs
-SET x_geomsource = 'Jan2017'
-WHERE
-	the_geom IS NOT NULL
-	AND x_geomsource IS NULL
-	AND x_datafreshness = 'January 2017';
-
--- Geoclient
-
--- Pull in Geoclient output
-UPDATE dobdev_jobs
-SET
-	the_geom = (CASE WHEN b.long_geoclient <> 0 THEN ST_SetSRID(ST_MakePoint(b.long_geoclient, b.lat_geoclient),4326) ELSE NULL END),
-	bbl = (CASE WHEN b.bbl_geoclient::text IS NOT NULL THEN b.bbl_geoclient::text ELSE bbl END),
-	bin = (CASE WHEN b.bin_geoclient::text IS NOT NULL THEN b.bin_geoclient::text ELSE bin END),
-	x_geomsource = (CASE WHEN b.long_geoclient <> 0 THEN 'Geoclient' ELSE NULL END)
-FROM dobdev_jobs_geoclient AS b
-WHERE dobdev_jobs.dob_job_number::text = b.dob_job_number::text;
- 
 -- GBAT
-
--- Apply geoms from Function A GBAT results
-UPDATE dobdev_jobs
+-- Apply geoms from Function A GBAT results. These geoms are inside the building.
+UPDATE dobdev_jobs_20180316
 SET
 	the_geom = ST_SetSRID(ST_MakePoint(b.Along, b.Alat),4326),
-	bbl = (CASE WHEN dobdev_jobs.bbl IS NULL THEN b.bbl::text ELSE dobdev_jobs.bbl END),
-	bin = (CASE WHEN dobdev_jobs.bin IS NULL THEN b.bin::text ELSE dobdev_jobs.bin END),
 	x_geomsource = 'GBAT-A'
 FROM dobdev_gbat_jobs AS b
 WHERE
-	dobdev_jobs.dob_job_number = b.dob_job_number::text
-	AND dobdev_jobs.the_geom IS NULL;
+	dobdev_jobs_20180316.dob_job_number = b.dob_job_number::text
+	AND b.alat IS NOT NULL;
 
--- Apply geoms from Function E GBAT results
-UPDATE dobdev_jobs
+-- Geoclient
+-- Pull in Geoclient BINS and BBLs
+UPDATE dobdev_jobs_20180316
 SET
-	the_geom = (CASE WHEN b.Elong <> 0 THEN ST_SetSRID(ST_MakePoint(b.Elong, b.Elat),4326) END),
-	bbl = (CASE WHEN dobdev_jobs.bbl IS NULL THEN b.bbl::text ELSE dobdev_jobs.bbl END),
-	bin = (CASE WHEN dobdev_jobs.bin IS NULL THEN b.bin::text ELSE dobdev_jobs.bin END),
-	x_geomsource = (CASE WHEN b.Elong <> 0 THEN 'GBAT-E' ELSE NULL END)
-FROM dobdev_gbat_jobs AS b
+	bbl = (CASE WHEN b.bbl_geoclient::text IS NOT NULL THEN b.bbl_geoclient::text ELSE bbl END),
+	bin = (CASE WHEN b.bin_geoclient::text IS NOT NULL THEN b.bin_geoclient::text ELSE bin END)
+FROM dobdev_jobs_geoclient AS b
+WHERE dobdev_jobs_20180316.dob_job_number::text = b.dob_job_number::text;
+
+UPDATE dobdev_jobs_20180316
+SET
+	the_geom = (CASE WHEN b.long_geoclient <> 0 THEN ST_SetSRID(ST_MakePoint(b.long_geoclient, b.lat_geoclient),4326) ELSE NULL END),
+	x_geomsource = (CASE WHEN b.long_geoclient <> 0 THEN 'Geoclient' ELSE NULL END)
+FROM dobdev_jobs_geoclient AS b
 WHERE
-	dobdev_jobs.dob_job_number = b.dob_job_number::text
-	AND dobdev_jobs.the_geom IS NULL;
+	dobdev_jobs_20180316.dob_job_number::text = b.dob_job_number::text
+	AND dobdev_jobs_20180316.the_geom IS NULL;
+
+UPDATE dobdev_jobs_20180316
+SET
+	the_geom = ST_Centroid(b.the_geom),
+	x_geomsource = 'Geoclient-BINCentoid'
+FROM dcpadmin.doitt_building_footprints_201802 AS b
+WHERE
+	dobdev_jobs_20180316.bin = b.bin::text
+	AND x_geomsource = 'Geoclient';
+
+UPDATE dobdev_jobs_20180316
+SET
+	the_geom = ST_Centroid(b.the_geom),
+	x_geomsource = 'Geoclient-BBLCentoid'
+FROM dcpadmin.dcp_mappluto_2017v1 AS b
+WHERE
+	dobdev_jobs_20180316.bbl = b.bbl::text
+	AND x_geomsource = 'Geoclient';
 
 -- PLUTO
-
 -- Pull in PLUTO centroid geom for remaining records without geometries
-UPDATE dobdev_jobs
+UPDATE dobdev_jobs_20180316
 SET
 	the_geom = ST_Centroid(b.the_geom),
 	x_geomsource = 'PLUTO'
 FROM dcpadmin.dcp_mappluto_2017v1 AS b
 WHERE
-	dobdev_jobs.the_geom IS NULL
-	AND dobdev_jobs.bbl= b.bbl::text;
+	dobdev_jobs_20180316.the_geom IS NULL
+	AND dobdev_jobs_20180316.bbl= b.bbl::text;
 
 -- Pull in PLUTO centroid geom for remaining records without geometries
-UPDATE dobdev_jobs
+UPDATE dobdev_jobs_20180316
 SET
 	the_geom = ST_Centroid(b.the_geom),
 	x_geomsource = 'PLUTO-App'
 FROM dcpadmin.dcp_mappluto_2017v1 AS b
 WHERE
-	dobdev_jobs.the_geom IS NULL
-	AND dobdev_jobs.bbl= b.appbbl::text;
+	dobdev_jobs_20180316.the_geom IS NULL
+	AND dobdev_jobs_20180316.bbl= b.appbbl::text;
 
 -- MANUAL
-
 -- Kristina/Population's manual geocoding
-UPDATE dobdev_jobs
+UPDATE dobdev_jobs_20180316
 SET
 	the_geom = ST_SetSRID(ST_MakePoint(b.ks_long::numeric, b.ks_lat::numeric),4326),
 	x_geomsource = 'Manual-Population'
 FROM dobdev_population_mangeo_20170615 AS b
 WHERE
-	dobdev_jobs.the_geom IS NULL
-	AND dobdev_jobs.dob_job_number = b.dob_job_number::text;
+	dobdev_jobs_20180316.the_geom IS NULL
+	AND dobdev_jobs_20180316.dob_job_number = b.dob_job_number::text;
 
 -- Jackie's manual geocoding
-UPDATE dobdev_jobs
+UPDATE dobdev_jobs_20180316
 SET
 	the_geom = ST_SetSRID(ST_MakePoint(b.Along::numeric, b.Alat::numeric),4326),
 	x_geomsource = 'Manual-Jackie'
 FROM dobdev_jackie_mangeo AS b
 WHERE
-	dobdev_jobs.the_geom IS NULL
-	AND dobdev_jobs.dob_job_number = b.dob_job_number::text;
+	dobdev_jobs_20180316.the_geom IS NULL
+	AND dobdev_jobs_20180316.dob_job_number = b.dob_job_number::text;
 
 -- Bill's manual geocoding
-UPDATE dobdev_jobs
+UPDATE dobdev_jobs_20180316
 SET
 	the_geom = ST_SetSRID(ST_MakePoint(b.Along::numeric, b.Alat::numeric),4326),
 	x_geomsource = 'Manual-Bill'
 FROM dobdev_bill_mangeo AS b
 WHERE
-	dobdev_jobs.the_geom IS NULL
-	AND dobdev_jobs.dob_job_number = b.dob_job_number::text;
+	dobdev_jobs_20180316.the_geom IS NULL
+	AND dobdev_jobs_20180316.dob_job_number = b.dob_job_number::text;
 
--- Final round of manual geocoding
-UPDATE dobdev_jobs
+-- Final round of manual geocoding: Bill's Legacy + Sam's
+UPDATE dobdev_jobs_20180316
 SET
 	the_geom = ST_SetSRID(ST_MakePoint(b.longitude::numeric, b.latitude::numeric),4326),
 	bin = b.bin,
@@ -116,23 +115,23 @@ SET
 	x_geomsource = (CASE WHEN b.label = 'Legacy' THEN 'Manual-BillLegacy' WHEN b.label = 'SamL-manual' THEN 'Manual-Sam' END)
 FROM dobdev_manualgeo_sl AS b
 WHERE
-	dobdev_jobs.the_geom IS NULL
-	AND dobdev_jobs.dob_job_number = b.dob_job_number::text;
+	dobdev_jobs_20180316.the_geom IS NULL
+	AND dobdev_jobs_20180316.dob_job_number = b.dob_job_number::text;
 
 -- Reapply reapply previous geoms from points that were manually moved
--- UPDATE dobdev_jobs
--- SET
--- 	the_geom = ST_SetSRID(ST_MakePoint(-73.96792173,40.7148463),4326),
--- 	x_geomsource = 'Manual-Move'
--- WHERE dob_job_number = '320917503';
+UPDATE dobdev_jobs_20180316
+SET
+	the_geom = ST_SetSRID(ST_MakePoint(-73.96792173,40.7148463),4326),
+	x_geomsource = 'Manual-Move'
+WHERE dob_job_number = '320917503';
 
--- UPDATE dobdev_jobs
--- SET
--- 	the_geom = ST_SetSRID(ST_MakePoint(-74.01180267,40.70082104),4326),
--- 	x_geomsource = 'Manual-Move'
--- WHERE dob_job_number = '121324129';
+UPDATE dobdev_jobs_20180316
+SET
+	the_geom = ST_SetSRID(ST_MakePoint(-74.01180267,40.70082104),4326),
+	x_geomsource = 'Manual-Move'
+WHERE dob_job_number = '121324129';
 
-UPDATE dobdev_jobs
+UPDATE dobdev_jobs_20180316
 SET
 	the_geom = ST_SetSRID(ST_MakePoint(-73.962936, 40.719756),4326),
 	x_geomsource = 'Manual-Move'
@@ -140,19 +139,19 @@ WHERE dob_job_number = '302143883';
 
 
 -- Clean up dummy BBLs and BINs
-UPDATE dobdev_jobs
+UPDATE dobdev_jobs_20180316
 SET bbl = NULL
 WHERE bbl = '0';
 
-UPDATE dobdev_jobs
+UPDATE dobdev_jobs_20180316
 SET bin = NULL
 WHERE bin = '0' OR RIGHT(bin, 6) = '000000';
 
 -- Calculate coordinates to fill in columns
-UPDATE dobdev_jobs
+UPDATE dobdev_jobs_20180316
 SET
-latitude = ST_Y(the_geom),
-longitude = ST_X(the_geom),
-ycoord = ST_Y(ST_Transform(the_geom, 2263)),
-xcoord = ST_X(ST_Transform(the_geom, 2263));
+	latitude = ST_Y(the_geom),
+	longitude = ST_X(the_geom),
+	ycoord = ST_Y(ST_Transform(the_geom, 2263)),
+	xcoord = ST_X(ST_Transform(the_geom, 2263));
 
--- 09_spatial_joins.sql
+++ 09_spatial_joins.sql
@@ -1,59 +1,59 @@
 -- 7.1
-UPDATE dobdev_jobs
+UPDATE dobdev_jobs_20180316
 	SET geo_mszone201718 = b.dbn
 	FROM dcpadmin.doe_school_zones_ms_2017 as b
-	WHERE ST_Intersects(dobdev_jobs.the_geom,b.the_geom); 
+	WHERE ST_Intersects(dobdev_jobs_20180316.the_geom,b.the_geom); 
 
-UPDATE dobdev_jobs
+UPDATE dobdev_jobs_20180316
 	SET geo_pszone201718 = b.dbn
 	FROM dcpadmin.doe_school_zones_es_2017 as b
-	WHERE ST_Intersects(dobdev_jobs.the_geom,b.the_geom); 
+	WHERE ST_Intersects(dobdev_jobs_20180316.the_geom,b.the_geom); 
 
 -- 7.2
-UPDATE dobdev_jobs
+UPDATE dobdev_jobs_20180316
 	SET
 		geo_csd = b.schooldist::text,
 		geo_subdistrict = b.distzone
 	FROM dcpadmin.doe_schoolsubdistricts as b
-	WHERE ST_Intersects(dobdev_jobs.the_geom,b.the_geom);
+	WHERE ST_Intersects(dobdev_jobs_20180316.the_geom,b.the_geom);
 
 -- 7.3
-UPDATE dobdev_jobs
+UPDATE dobdev_jobs_20180316
 	SET
 		geo_ntacode = b.ntacode,
 		geo_ntaname = b.ntaname
 	FROM dcpadmin.support_admin_ntaboundaries as b
-	WHERE ST_Intersects(dobdev_jobs.the_geom,b.the_geom); 
+	WHERE ST_Intersects(dobdev_jobs_20180316.the_geom,b.the_geom); 
 
-UPDATE dobdev_jobs
+UPDATE dobdev_jobs_20180316
 	SET geo_cd = b.borocd::text
 	FROM dcpadmin.nyc_communitydistricts as b
-	WHERE ST_Intersects(dobdev_jobs.the_geom,b.the_geom);
+	WHERE ST_Intersects(dobdev_jobs_20180316.the_geom,b.the_geom);
 
 -- 7.4
-UPDATE dobdev_jobs
+UPDATE dobdev_jobs_20180316
 	SET geo_censusblock = b.bctcb2010
 	FROM dcpadmin.nyc_census_blocks_2010_wi as b
-	WHERE ST_Within(dobdev_jobs.the_geom,b.the_geom); 
+	WHERE ST_Within(dobdev_jobs_20180316.the_geom,b.the_geom); 
 
 -- 7.5
-UPDATE dobdev_jobs
+UPDATE dobdev_jobs_20180316
 	SET f_firms2007_100yr = b.fld_zone
 	FROM dcpadmin.fema_firms2007_100yr as b
-	WHERE ST_Intersects(dobdev_jobs.the_geom,b.the_geom);
+	WHERE ST_Intersects(dobdev_jobs_20180316.the_geom,b.the_geom);
 
-UPDATE dobdev_jobs
+UPDATE dobdev_jobs_20180316
 	SET f_pfirms2015_100yr = b.fld_zone
 	FROM dcpadmin.fema_pfirms_100yr_2015 as b
-	WHERE ST_Intersects(dobdev_jobs.the_geom,b.the_geom);
+	WHERE ST_Intersects(dobdev_jobs_20180316.the_geom,b.the_geom);
 
 -- 7.6
-UPDATE dobdev_jobs
+UPDATE dobdev_jobs_20180316
 	SET f_2050s_100yr = 'Within 2050s 100yr floodplain'
 	FROM dcpadmin.sealevel_2050s_100yr as b
-	WHERE ST_Intersects(dobdev_jobs.the_geom,b.the_geom);
+	WHERE ST_Intersects(dobdev_jobs_20180316.the_geom,b.the_geom);
 
-UPDATE dobdev_jobs
+UPDATE dobdev_jobs_20180316
  	SET f_2050s_hightide = 'Within 2050s high tide 30in'
  	FROM dcpadmin.sealevel_2050s_hightide as b
- 	WHERE ST_Intersects(dobdev_jobs.the_geom,b.the_geom);
+ 	WHERE ST_Intersects(dobdev_jobs_20180316.the_geom,b.the_geom);
